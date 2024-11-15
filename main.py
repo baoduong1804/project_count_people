95e5a068ac0494f0c8080ba3da3027c619b3ac84
baoduong <baoduong.18042k3@gmail.com>
2024-11-16 03:42:57 +0700

    first commit

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import cv2
 import pandas as pd
 import numpy as np
 from ultralytics import YOLO
-
+from tracker import*
 
 model=YOLO('yolov8s.pt')
 
+#Vẽ hai vùng 
+# area1=[(312,388),(289,390),(474,469),(497,462)]
+# area2=[(279,392),(250,397),(423,477),(454,469)]
 
-area1=[(312,388),(289,390),(474,469),(497,462)]
+# area2=[(120,540),(90,570),(260,610),(290,570)]
+# area1=[(279,392),(250,397),(423,477),(454,469)]
 
-area2=[(279,392),(250,397),(423,477),(454,469)]
+
+
+area1=[(120,470),(90,510),(330,550),(350,505)]
+area2=[(80,530),(50,570),(310,610),(330,570)]
+
+
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE :  
         colorsBGR = [x, y]
@@ -19,7 +28,7 @@ def RGB(event, x, y, flags, param):
 cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
 
-cap=cv2.VideoCapture('peoplecount1.mp4')
+cap=cv2.VideoCapture('video10.mp4')
 
 
 my_file = open("coco.txt", "r")
@@ -29,6 +38,12 @@ class_list = data.split("\n")
 
 count=0
 
+tracker = Tracker()
+
+people_upping = {}# chứa các id và tọa độ người đi lên
+people_downing = {}# chứa các id và tọa độ người đi xuống
+upping = set()#chứa id người đi lên
+downing = set()#chứa id người đi xuống
 
 while True:    
     ret,frame = cap.read()
@@ -37,7 +52,8 @@ while True:
     count += 1
     if count % 2 != 0:
         continue
-    frame=cv2.resize(frame,(1020,500))
+    #Kích thước video
+    frame=cv2.resize(frame,(480 ,640))
 #    frame=cv2.flip(frame,1)
     results=model.predict(frame)
  #   print(results)
@@ -56,19 +72,58 @@ while True:
         d=int(row[5])
         c=class_list[d]
         if 'person' in c:
-           cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-           cv2.putText(frame,str(c),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,(0.5),(255,255,255),1)
-        
-      
+            list.append([x1,y1,x2,y2])
+            
+    bbox_id = tracker.update(list)
+    for bbox in bbox_id :
+        x3,y3,x4,y4,id = bbox
+        ##people up
+        results = cv2.pointPolygonTest(np.array(area2,np.int32),(((x4+x3)//2,y4)),False) #chấm tròn vào trong khu vực hai thì reutrn 1.0
+        cv2.rectangle(frame,(x3,y3),(x4,y4),(0,255,0),2) # Vẽ hình chữ nhật
+        cv2.circle(frame,((x4+x3)//2 ,y4),5,(255,0,255),-1) # vẽ chấm tròn nhỏ phía dưới khung
+        cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,(0.5),(255,255,255),1)
+        if results >= 0 :
+            people_upping[id] = (x4,y4)
+            
+        if id in people_upping :
+            results1 = cv2.pointPolygonTest(np.array(area1,np.int32),(((x4+x3)//2,y4)),False)
+            if results1 >= 0 :
+                # cv2.rectangle(frame,(x3,y3),(x4,y4),(0,255,0),2)# Vẽ hình chữ nhật
+                # cv2.circle(frame,((x4+x3)//2,y4),5,(255,0,255),-1) # vẽ chấm tròn nhỏ phía dưới khung
+                # cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,(0.5),(255,255,255),1)
+                upping.add(id)
+                
+        ##people down
+        results2 = cv2.pointPolygonTest(np.array(area1,np.int32),(((x4+x3)//2,y4)),False) #chấm tròn vào trong khu vực hai thì reutrn 1.0
+        if results2 >= 0 :
+            people_downing[id] = (x4,y4)
+        if id in people_downing :
+            results3 = cv2.pointPolygonTest(np.array(area2,np.int32),(((x4+x3)//2,y4)),False)
+            if results3 >= 0 :
+                # cv2.rectangle(frame,(x3,y3),(x4,y4),(0,255,0),2)# Vẽ hình chữ nhật
+                # cv2.circle(frame,((x4+x3)//2,y4),5,(255,0,255),-1) # vẽ chấm tròn nhỏ phía dưới khung
+                # cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,(0.5),(255,255,255),1)
+                downing.add(id)
+
             
             
         
-    cv2.polylines(frame,[np.array(area1,np.int32)],True,(255,0,0),2)
-    cv2.putText(frame,str('1'),(504,471),cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,0,0),1)
+    cv2.polylines(frame,[np.array(area1,np.int32)],True,(0,0,255),2)
+    cv2.putText(frame,str('1'),area1[0],cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,255,0),1)
 
     cv2.polylines(frame,[np.array(area2,np.int32)],True,(255,0,0),2)
-    cv2.putText(frame,str('2'),(466,485),cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,0,0),1)
+    cv2.putText(frame,str('2'),area2[0],cv2.FONT_HERSHEY_COMPLEX,(0.5),(0,255,0),1)
 
+    #print count people
+    
+    print(f"di len: {len(upping)}")
+    print(f"di len: {len(downing)}")
+    up = (len(upping))
+    down =(len(downing))
+    cv2.putText(frame,str(f"Di len: {up}"),(20,40),cv2.FONT_HERSHEY_COMPLEX,(0.7),(0,255,0),2)
+    cv2.putText(frame,str(f"Di xuong: {down}"),(20,80),cv2.FONT_HERSHEY_COMPLEX,(0.7),(0,255,0),2)
+    
+    
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
         break
